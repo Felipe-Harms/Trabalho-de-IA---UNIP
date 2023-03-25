af02ae2b8af88ae9814a70957c7c4f84e7db66ff
Felipe-harms` <Felipe.freitas28@aluno.unip.br`>
2023-03-25 12:30:34 -0300

    att variável "achou"

--- Profundidade.py
+++ Profundidade.py
@@ -9,17 +9,22 @@ class Profundidade:
         self.objetivo = objetivo
         self.fronteira = Pilha(20)
         self.fronteira.empilhar(inicio)
+        self.achou = False
 
     def buscar(self):
         topo = self.fronteira.getTopo()
         print("Topo: {}".format(topo.nome))  # type: ignore
 
-        for a in topo.adjacentes:  # type: ignore
-            print("Verificando se {} já foi visitado".format(a.cidade.nome))
-            if a.cidade.visitado == False:
-                a.cidade.visitado = True
-                self.fronteira.empilhar(a.cidade)
-                Profundidade.buscar(self)
+        if topo == self.objetivo:
+            self.achou = True
+        else:
+            for a in topo.adjacentes:  # type: ignore
+                if self.achou == False:
+                    print("Verificando se {} já foi visitado".format(a.cidade.nome))
+                    if a.cidade.visitado == False:
+                        a.cidade.visitado = True
+                        self.fronteira.empilhar(a.cidade)
+                        Profundidade.buscar(self)
         print("Desempilhando {}".format(
             self.fronteira.desempilhar().nome))  # type: ignore
 
